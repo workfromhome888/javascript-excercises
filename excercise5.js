8a67808eb25f3f182b6f402b145c02ceb2732eaf
reyzr <rey.zr@hotmail.com>
2020-02-07 19:03:58 -0800

    Excercises js

---
+++ excercise5.js
@@ -0,0 +1,11 @@
+// excercise5.js
+
+
+var name = 'Jane';
+var age = 20;
+console.log(name);
+console.log(age);
+
+// * Create a new index5.js file
+// * Copy and paste the code from ex_4
+// // * Show each variable value using `console.log()`
